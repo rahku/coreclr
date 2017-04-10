c02c06ba5b684885ed54aab324347713c5c17b76
Rahul Kumar <rahku@microsoft.com>
2017-04-10 13:37:26 -0700

    temp uwp port

---
+++ src/coreclr/UWP/Host/UAPHost/UAPHost.cpp
@@ -0,0 +1,6 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+#pragma comment(linker, "/export:DllGetActivationFactory=uwphost.DllGetActivationFactory")
+#pragma comment(linker, "/export:ExecuteAssembly=uwphost.ExecuteAssembly")
